d8ef3732c1da69f3fd70c52b85032f4c976ea937
alios <abc.ua@yandex.ru>
2014-02-07 02:04:09 +0200

    up

--- dropdown.js
+++ dropdown.js
@@ -5,19 +5,19 @@
 
     $(function() {
         $('.dropdown').each(function() {
-            var _this = this,
-                $content = $(this).find('.dropdown__content-wrapper').eq(0);
+            var $this = $(this),
+                $content = $this.find('.dropdown__content-wrapper').eq(0);
 
             $(document.body).click(function() {
                 hide($content);
             });
 
-            $(this).click(function() {
+            $this.click(function() {
                 return false;
             }).on('click', '.dropdown__item', function() {
                     hide($content);
 
-                    $(_this).trigger('select', [$(this).text()]);
+                    $this.trigger('select', [$(this).text()]);
                 }).find('.dropdown__switcher').click(function() {
                     $content.toggleClass('dropdown__content-wrapper_visibility_visible');
                 });
